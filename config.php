68aed2731c29a99c9bd5b857c3d13131602073e2
jon-by <yure.df@hotmail.com>
2020-06-14 17:30:09 -0300

    Organização e estrutura de rotas

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+/**
+ * Timezone e 
+ */
+
+setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+
+date_default_timezone_set('America/Sao_Paulo');
+
+
+
+
+
+
