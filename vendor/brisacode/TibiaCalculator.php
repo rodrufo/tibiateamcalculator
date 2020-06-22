c058e705c3f4d8ff1601b83dd9a5b64b8f51ddcd
Rodolfo <paoluccifaetec@gmail.com>
2020-06-22 14:43:04 -0300

    Implementação da calculadora que informa os pagamentos para igualar os balances da pt.

--- vendor/brisacode/TibiaCalculator.php
+++ vendor/brisacode/TibiaCalculator.php
@@ -2,80 +2,76 @@
 
 namespace brisacode;
 
-class TibiaCalculator extends TibiaDataExtractor{
+class TibiaCalculator extends TibiaDataExtractor {
 
-private $payments;
+    private $partyData;
 
+    public function payments() {
 
+        $extractedData = new TibiaDataExtractor($this->getPartyData());
+        $calculator = array(
+            'balance' => 0,
+            'individualBalance' => 0,
+            'pay' => array(),
+            'payto' => array()
+        );
+        $payments = array();
 
+        foreach ($extractedData->getExtractedSessionData()['players'] as $i => $player)
+            $calculator['balance'] += $player['balance'];
 
+        if ($calculator['balance'] != 0)
+            $calculator['individualBalance'] = $calculator['balance'] / $extractedData->getNumberOfPlayers();
 
+        foreach ($extractedData->getExtractedSessionData()['players'] as $i => $player)
+            $calculator['pay'][$player['nome']] = $calculator['individualBalance'] - $player['balance'];
 
-/**
- * Get the value of payments
- */ 
-public function getPayments()
-{
-return $this->payments;
+        foreach ($calculator['pay'] as $nome => $individualBalanceCorrigido)
+            if ($individualBalanceCorrigido < 0)
+                $calculator['payto'][$nome] = $individualBalanceCorrigido;
+
+        foreach ($calculator['pay'] as $nome => $individualBalanceCorrigido)
+            if ($individualBalanceCorrigido > 0) {
+                foreach ($calculator['payto'] as $nomePlayerPositivo => $individualBalanceCorrigidoPayto) {
+                    if ($individualBalanceCorrigido > 0 && $individualBalanceCorrigidoPayto != 0) {
+                        if ((-1 * $individualBalanceCorrigidoPayto) > $individualBalanceCorrigido) {
+                            $payments[] = array(
+                                'pagador' => $nomePlayerPositivo,
+                                'valor' => round($individualBalanceCorrigido),
+                                'recebedor' => $nome
+                            );
+                            $calculator['payto'][$nomePlayerPositivo] = $individualBalanceCorrigidoPayto + $individualBalanceCorrigido;
+                            $individualBalanceCorrigido = 0;
+                        } else {
+                            $payments[] = array(
+                                'pagador' => $nomePlayerPositivo,
+                                'valor' => round(-1 * $individualBalanceCorrigidoPayto),
+                                'recebedor' => $nome
+                            );
+                            $calculator['payto'][$nomePlayerPositivo] = 0;
+                            $individualBalanceCorrigido += $individualBalanceCorrigidoPayto;
+                        }
+                    }
+                }
+            }
+
+        return array(
+            'balance' => $calculator['balance'],
+            'individualBalance' => round($calculator['individualBalance']),
+            'payments' => $payments
+        );
+    }
+
+    public function setPartyData($data) {
+        $this->partyData = $data;
+    }
+
+    public function getPartyData() {
+        return $this->partyData;
+    }
+
+    public function __construct($partyData) {
+        $this->setPartyData($partyData);
+    }
+
 }
-
-/**
- * Set the value of payments
- *
- * @return  self
- */ 
-public function setPayments($payments)
-{
-$this->payments = $payments;
-
-return $this;
-}
-
-
-
-
-public function payments(){
-
-$profit = $this->getTotalBalance();
-
-return $profit;
-
-if( $profit > 0 ){    
-
-}else{
-
-}
-
-
-
-
-
-}
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
