676ceb63999385566d2a77f6954878ce652fdaf7
Rodolfo <paoluccifaetec@gmail.com>
2020-10-29 23:52:50 -0300

    Atualização das regras de boss

--- app/views/cache/boss.60462b3dd789b19393820db07b239780.rtpl.php
+++ app/views/cache/boss.60462b3dd789b19393820db07b239780.rtpl.php
@@ -1,7 +1,28 @@
 <?php if(!class_exists('Rain\Tpl')){exit;}?><section class="regra">
     <h1>Regras Grupo Aberto de Boss (21/10/2020)</h1>
-    <h4>As regras a seguir complementam o manifesto, mas são específica para o grupo de boss organizados pela Guild.</h4>
+    <h4>As regras a seguir complementam o manifesto e abrangem o grupo de boss organizados pela Guild no whatsapp.</h4>
     <p>1. Listas para King Zelos serão passadas na noite anterior, logo após encerramento do zelos.</p>
-    <p>2. Em bosses e quests, caso não possa comparecer no horário marcado, o player deverá remover o nome da lista (seja titular ou reserva) com no mínimo 30 minutos de antecedência do horário programado para execução do boss/quest, para que haja possibilidade de substituição.</p>
-    <p>3. O membro que colocar nome nas listas (seja titular ou reserva) e não comparecer no horário marcado, sem justificar com antecedência mínima de 30 min, por duas vezes em um período de dois meses ficará impedido de participar dos bosses diários por 15 dias. Caso esse comportamento aconteça em boss de 14 dias, o membro ficará impedido de participar da próxima lista do mesmo boss.</p>
+    <p>2. Em bosses e quests, caso não possa comparecer no horário marcado, o player deverá remover o nome da lista (seja titular ou reserva) com no mínimo 30 minutos de antecedência do horário programado para execução do boss/quest, para que haja possibilidade de substituição.
+    </p>
+    <p>3. O membro que colocar nome nas listas (seja titular ou reserva) e não comparecer no horário marcado, com até 10 minutos de tolerância, sem justificar com antecedência mínima de 30 minutos, por duas vezes em um período de dois meses ficará impedido de participar dos bosses diários por 15 dias. Caso esse comportamento aconteça em boss de 14 dias, o membro ficará impedido de participar da próxima lista do mesmo boss.
+    </p>
+    <p>4. O print contendo a aba loot, o supply analyser e o loot analyser deve ser tirado pelo computador. Foto da tela não será considerada print.
+    </p>
+    <p>5. Se o membro morrer durante o boss, deve manter o char online até a finalização do boss, e enviar print normalmente (aba loot, suply analyser e loot analyser)   
+    </p>
+    <p>6. Caso a print não seja enviada corretamente, o time do Boss deverá decidir o quê fazer. Se não houver consenso, líderes ou vice líderes da guild Brisa presente devem tomar uma decisão sobre o caso.
+    </p>
+    <h1>Reglas de Boss Open Group (21/10/2020)</h1>
+    <h4>Las siguientes reglas complementan el manifiesto y aplican al grupo de bosses que organiza la guild en whatsapp.</h4>
+    <p>1. Las listas de King Zelos se pasarán la noche anterior, justo después de que termine el Zelos del día.</p>
+    <p>2. En bosses y misiones, si el jugador no puede aparecer a la hora programada, el jugador debe eliminar el nombre de la lista (ya sea titular o reserva) al menos 30 minutos antes de la hora programada para la ejecución del boss / misión, para que exista la posibilidad de sustitución.
+    </p>
+    <p>3. El miembro que ponga un nombre en las listas (ya sea titular o reserva) y no se presente a la hora programada, sin justificar con al menos 30 minutos de anticipación, dos veces en un período de dos meses, no podrá participar en los bosses diarios durante 15 días. Si este comportamiento ocurre en un boss de 14 días, el miembro no podrá participar en la siguiente lista del mismo boss.
+    </p>
+    <p>4. La print debe ser tomada por la computadora desde el cliente, conteniendo: la pestaña de loot, el loot analyzer y el supply analyzer. Tomar foto de la pantalla no se considerará válido.
+    </p>
+    <p>5. Si alguien muere durante el boss, debe mantener al personaje en línea hasta que el boss termine y enviar una print normalmente (pestaña de loot, supply analyzer y loot analyzer)
+    </p>
+    <p>6. Si la print no se envía correctamente, el equipo de Boss debe decidir qué hacer. Si no hay consenso, los líderes o vice líderes del guild Brisa presentes deben tomar una decisión sobre el caso.
+    </p>
 </section>
